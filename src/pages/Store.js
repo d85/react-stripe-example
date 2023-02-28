8ee8134a79628273246c9101b324d0ef21a02ccb
Dan Carlier <dan.carlier@gmail.com>
2023-02-28 11:45:43 +1100

    Add ProductCart component

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,14 +1,15 @@
 import { Row, Col } from 'react-bootstrap';
 import { productsArray } from '../productsStore';
+import ProductCard from '../components/ProductCard';
 
 function Store() {
   return (
     <>
-      <h1>Welcome to the Store!</h1>
+      <h1 align="center" className="p-3">Welcome to the Store!</h1>
       <Row xs={1} md={3} className="g-4">
         {productsArray.map((product, idx) => (
           <Col align="center" key={idx}>
-            <h1>{product.title}</h1>
+            <ProductCard product={product}/>
           </Col>
         ))}
       </Row>
